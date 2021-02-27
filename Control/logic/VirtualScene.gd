94012ea6cd4da72591b3e67af6821e194ef8d9dc
FremontTeng <fremontteng@gmail.com>
2021-02-28 00:28:21 +0800

    Added audio recorder

--- Control/logic/VirtualScene.gd
+++ Control/logic/VirtualScene.gd
@@ -1,20 +1,21 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# Variables
+var effect
+var recording
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# We get the index of the "Record" bus.
+	var idx = AudioServer.get_bus_index("Record")
+	# And use it to retrieve its first effect, which has been defined
+	# as an "AudioEffectRecord" resource.
+	effect = AudioServer.get_bus_effect(idx, 0)
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://View/Scenes/MainScreen.tscn")
@@ -38,6 +39,14 @@ func _on_Listen_pressed():
 	$Control/TextContainer/TextEdit.text = "Penguin: Am blushing~"
 	var animated = $Control/Character/Penguin/Animation
 	animated.play("blushing")
+	#For recording sounds
+	if effect.is_recording_active():
+		recording = effect.get_recording()
+		effect.set_recording_active(false)
+		$MarginContainer/VBoxContainer/TopButtons/Listen/Label.text = "Record"
+	else:
+		effect.set_recording_active(true)
+		$MarginContainer/VBoxContainer/TopButtons/Listen/Label.text = "Stop"
 	pass # Replace with function body.
 
 
@@ -45,4 +54,20 @@ func _on_Animation_animation_finished():
 	$Control/TextContainer/TextEdit.text = "Penguin: Talk to me!"
 	var animated = $Control/Character/Penguin/Animation
 	animated.play("default")
+	pass # Replace with function body.
+
+
+func _on_Play_pressed():
+	print(recording)
+	print(recording.format)
+	print(recording.mix_rate)
+	print(recording.stereo)
+	var data = recording.get_data()
+	print(data)
+	print(data.size())
+	$AudioStreamPlayer.stream = recording
+	$AudioStreamPlayer.play()
+	$Control/TextContainer/TextEdit.text = "Penguin: Am blushing~"
+	var animated = $Control/Character/Penguin/Animation
+	animated.play("blushing")
 	pass # Replace with function body.
