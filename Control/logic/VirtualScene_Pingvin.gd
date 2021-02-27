16777510c7628baaeb5fc31a893b58a43badf59b
FremontTeng <fremontteng@gmail.com>
2021-02-28 04:22:33 +0800

    Fixed convos from pingvin

--- Control/logic/VirtualScene_Pingvin.gd
+++ Control/logic/VirtualScene_Pingvin.gd
@@ -1,21 +1,28 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+#Variables
+var speech
+var animated
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	animated = $Control/Character/Pingvin/Animation
+	speech = $Control/TextContainer/TextEdit
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://View/Scenes/FriendsScene.tscn")
+	pass # Replace with function body.
+
+
+func _on_Greet_pressed():
+	speech.text = "Pingvin: Good afternoon~"
+	animated.play("blush")
+	pass # Replace with function body.
+
+
+func _on_Animation_animation_finished():
+	speech.text = "Pingvin was fed an hour ago."
+	animated.play("default")
 	pass # Replace with function body.
